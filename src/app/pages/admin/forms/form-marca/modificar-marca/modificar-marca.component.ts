d793b9dd989ff42ac90e4bd15b89fe61e373a6ba
NOTSTEVEN4000 <bsgp1986@gmail.com>
2023-01-15 12:51:57 -0500

    update

--- src/app/pages/admin/forms/form-marca/modificar-marca/modificar-marca.component.ts
+++ src/app/pages/admin/forms/form-marca/modificar-marca/modificar-marca.component.ts
@@ -8,23 +8,23 @@ import { ConexMarcaService,Marca } from 'src/app/services/conexiones/conex-marca
 })
 export class ModificarMarcaComponent implements OnInit {
   detalle:any={};
+  
   cargar:any={
     id:0,
     name:'',
     email:''
     
+    
   };
+
   
   constructor( private conexion:ConexMarcaService) {
 
     this.conexion.disparadorDetalle.subscribe(data=>{
        this.conexion.getUnmARCA(data).subscribe(
         res=>{
-          console.log(res)
-         
-          this.cargar=<any>res;
-        
-               
+          console.log(res)         
+          this.cargar=<any>res;               
         },
         err => console.log('Hola')
        );
@@ -38,25 +38,25 @@ export class ModificarMarcaComponent implements OnInit {
     nombre:'',
     descripcion:''
 } 
-id_entrada:number=0;
+  id_entrada:number=0;
+
   ngOnInit(): void { 
+   
   }
   
   modificar(id:number,nombre:string,descripcion:string){
-
+    //Extrae text//
     this.marca.id_Marca = id;
     this.id_entrada =id;
     this.marca.nombre = nombre;
     this.marca.descripcion = descripcion;
   
-   this.conexion.editmarca(this.id_entrada,this.marca).subscribe(
+    //Envia a la base de datos
+    this.conexion.editmarca(this.id_entrada,this.marca).subscribe(
        res=>{
-         console.log(res);
-       
+         console.log(res);       
        },
        err=>console.log(err)
-     );
-     
- }
-    
+     );   
+  } 
 }
